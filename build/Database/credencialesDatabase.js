92b1f374e3240271796508daf7b74a1c702d7837
StraightUpCode <sanchez.roberto0810@gmail.com>
2019-10-22 09:36:11 -0600

    Quite la carpeta build

--- build/Database/credencialesDatabase.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    host: 'localhost',
-    user: 'root',
-    password: 'admin',
-    database: 'taller'
-};
-//# sourceMappingURL=credencialesDatabase.js.map
