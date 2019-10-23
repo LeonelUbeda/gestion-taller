d49e0c4073cb992e6ecc1bc8442ddef4b4598ec8
StraightUpCode <sanchez.roberto0810@gmail.com>
2019-10-22 19:36:44 -0600

    hacer que passen los test

--- src/sequealize_mock_utils/MockModel.ts
+++ src/sequealize_mock_utils/MockModel.ts
@@ -1,13 +1,20 @@
+import { type } from "os"
 
 export class MockModel<T> {
     private _results: T[] = []
-    public addResults(...arr) {
-        this._results = arr
+    public addResults(array, ...spread){
+        if (spread.length > 0) {
+            this._results = [array, ...spread]
+        } else {
+            this._results = array
+        }
     }
-
-    private seleccion(where? ) {
+    public getResult(): T[] {
+        return this._results
+    }
+    private seleccion(where? ){
         let resultado = []
-        if ( Object.keys(where).length === 0) {
+        if (!where || Object.keys(where).length === 0) {
             resultado = this._results
         } else {
             return []
@@ -27,19 +34,24 @@ export class MockModel<T> {
             return nuevoObjeto
         })
     }
-    private find(options) {
-        const { attributes, where } = options
-        let results =this.seleccion(where) 
-        if (attributes) {
-            results = this.proyeccion(attributes, results)
+    private find(options?){
+        if (options) {
+            const {attributes , where} = options
+            let results = this.seleccion(where)
+            if (attributes) {
+                results = this.proyeccion(attributes, results)
+            }
+            return results
         }
-        return results
+        return this.seleccion()
+        
     }
-    public findOne(options) {
-        const result = this.find(options)
-        return result[0]
+    public findOne(options?) : T[] {
+        const result = options ? this.find() : this.find(options)
+        console.log(result)
+        return [ result[0]]
     }
-    findAll(options) {
+    findAll(options?) : T[]{
         return this.find(options)
     }
 
