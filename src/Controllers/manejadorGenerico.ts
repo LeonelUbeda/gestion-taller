d65dafa0e986719077f4874137a2ad60386bcc15
Leonel Ubeda <leonelubeda@hotmail.com>
2019-11-05 02:35:38 -0600

    Manejador de rutas generico casi terminado. CRUD Categoria terminado

--- src/Controllers/manejadorGenerico.ts
+++ src/Controllers/manejadorGenerico.ts
@@ -1,7 +1,8 @@
-import { factoryModelNuevo, factoryModelTodos, factoryModelActualizarId } from '../Controllers/genericos.controller'
+import { factoryModelNuevo, factoryModelTodos, factoryModelActualizarId, factoryModelEliminarCondicionAnd } from './genericos.controller'
 import {Request, Response} from 'express'
 
-const generico = ({modelo, accion, include = []}) => {
+
+const manejadorGenerico = ({modelo, accion, include = []}) => {
     const HTTP = {
         leer: async (req: Request, res: Response) => {
             const consulta = req.query
@@ -11,6 +12,17 @@ const generico = ({modelo, accion, include = []}) => {
                 res.status(200).json(resultado)
             } catch (error) {
                 res.status(400).json({mensaje: 'Error'})
+            }
+        },
+        leerId: async (req: Request, res: Response) => {
+            const {id} = req.params
+            const modeloTodosId = factoryModelTodos({modelo})
+            try {
+                const resultado = await modeloTodosId({id})
+                res.status(200).json(resultado)
+            } catch (error) {
+                res.status(400).json({mensaje: 'Error'})
+                
             }
         },
         crear: async (req: Request, res: Response) => {
@@ -28,10 +40,21 @@ const generico = ({modelo, accion, include = []}) => {
             const elemento = req.body;
             const modeloActualizar = factoryModelActualizarId({modelo})
             try {
-                const resultado = await factoryModelActualizarId({id, ...elemento})
+                const resultado = await modeloActualizar({id, ...elemento})
                 res.status(201).json(resultado)
             } catch (error) {
                 res.status(400).json({mensaje: 'Error'})
+            }
+        },
+        eliminarPorId: async (req: Request, res: Response) => {
+            const {id} = req.params
+            const modeloEliminar = factoryModelEliminarCondicionAnd({modelo})
+            try {
+                const resultado = await modeloEliminar({id})
+                res.status(200).json(resultado)
+            } catch (error) {
+                res.status(400).json({mensaje: 'Error'})
+                
             }
         }
         
@@ -39,13 +62,18 @@ const generico = ({modelo, accion, include = []}) => {
     }
     
     return HTTP[accion]
+    
 }
 
-generico.leer = 'leer'
-generico.crear = 'crear'
-generico.actualizar = 'actualizar'
-generico.eliminar = 'eliminar'
-generico.actualizarPorId = 'actualizarPorId'
+manejadorGenerico.LEER =                'leer'
+manejadorGenerico.LEER_POR_ID =         'leerId'
+manejadorGenerico.CREAR =               'crear'
+manejadorGenerico.ACTUALIZAR =          'actualizar'
+manejadorGenerico.ELIMINAR =            'eliminar'
+manejadorGenerico.ELIMINAR_POR_ID =     'eliminarPorId'
+manejadorGenerico.ACTUALIZAR_POR_ID =   'actualizarPorId'
+
+
 export default generico
 
 
--- src/Routes/Servicio/Categoria.ts
+++ src/Routes/Servicio/Categoria.ts
@@ -1,79 +1,18 @@
 import {Router, Response, Request} from 'express'
 
 const router = Router();
-import { categoriaTodos, categoriaId, categoriaNuevo, categoriaActualizar } from '../../Controllers/Servicio/categoria.controller'
+
 import Categoria from '../../Models/Servicio/Categoria';
-import generico from '../../Controllers/generico'
+import manejadorGenerico from '../../Controllers/manejadorGenerico'
 
 // Ruta generica para buscar roles
 
 
-
-
-router.post('/', generico({modelo: Categoria, accion: generico.crear}))
-router.get('/', generico({modelo: Categoria, accion: generico.leer}))
-router.put('/:id', generico({modelo: Categoria, accion: generico.actualizarPorId}))
-
-
-
-
-
-
-/*
-router.get('/', async (req: Request, res: Response) => {
-    const consulta = req.query
-    try {
-        const resultado = await categoriaTodos(consulta)
-        res.status(200).json(resultado)
-    } catch (error) {
-        res.status(400).json({mensaje: 'Error'})
-    }
-});
-
-
-
-*/
-
-
-
-
-router.get('/:id', async (req: Request, res: Response) => {
-    const {id} = req.params
-    try {
-        const resultado = await categoriaId(parseInt(id))
-        res.status(200).json(resultado)
-    } catch (error) {
-        res.status(400).json({mensaje: 'Error'})
-    }
-});
-
-// Crear nuevo Rol.     Obligatorio: nombre
-router.post('/', async (req: Request, res: Response) => {
-    const elemento = req.body
-    try {
-        const resultado = await categoriaNuevo({...elemento})
-        res.status(201).json(resultado) 
-    } catch (error) {
-        res.status(400).json({mensaje: 'Error'})
-    }
-})
-
-
-
-// Actualizar un rol.   Obligatorio: id     Opcional: campos a actualizar
-router.put('/:id', async (req: Request, res: Response) => {
-    const elemento = req.body
-    const { id } = req.params
-    try {
-        const resultado = await categoriaActualizar({id, ...elemento})
-        res.status(201).json(resultado)
-    } catch (error) {
-        res.status(400).json({mensaje: 'Error'})
-    }
-})
-
-
-
+router.get('/',         manejadorGenerico({modelo: Categoria,     accion: manejadorGenerico.LEER}))
+router.get('/:id',      manejadorGenerico({modelo: Categoria,     accion: manejadorGenerico.LEER_POR_ID}))
+router.post('/',        manejadorGenerico({modelo: Categoria,     accion: manejadorGenerico.CREAR}))
+router.put('/:id',      manejadorGenerico({modelo: Categoria,     accion: manejadorGenerico.ACTUALIZAR_POR_ID}))
+router.delete('/:id',   manejadorGenerico({modelo: Categoria,     accion: manejadorGenerico.ELIMINAR_POR_ID }))
 
 
 
