af2131f67373fe17e74bfd69855c0884722fe3d6
Carlos Dinarte <cdinarte8@gmail.com>
2019-11-05 22:09:20 -0600

    Añadir nuevo, buscar, actualizar a controlador herramienta

--- src/Controllers/Inventario/herramienta.controller.ts
+++ src/Controllers/Inventario/herramienta.controller.ts
@@ -1,36 +1,40 @@
-import {factoryModelNuevo, factoryModelActualizarId, factoryModelEliminarCondicionAnd, factoryModelTodos, factoryModelID, factoryModelActualizarPorCampo} from '../genericos.controller'
-import Herramienta from '../../Models/Inventario/Herramienta'
+import {factoryModelNuevo, factoryModelActualizarId, factoryModelEliminarCondicionAnd, factoryModelTodos, factoryModelID, factoryModelActualizarPorCampoRestriccion} from '../genericos.controller'
+import { Herramienta, MovimientoHerramienta } from '../../Models/Inventario/Herramienta'
 import {Rol, Permiso, RolPermiso} from '../../Models/Usuario/RolPermiso'
 
-// Usuario
+// Terminados
 export const herramientaTodos = factoryModelTodos({modelo: Herramienta})
 export const herramientoID = factoryModelID({modelo: Herramienta})
-export const herramientaNuevo = factoryModelNuevo({modelo: Usuario})
-//export const usuarioActualizar = factoryModelActualizarId({modelo: Usuario})
-export const usuarioEliminar = factoryModelEliminarCondicionAnd({modelo: Usuario})
+export const herramientaActualizar = factoryModelActualizarPorCampoRestriccion({modelo: Herramienta}) // esto no permite actualizar cantidad
 
-export const usuarioActualizar = factoryModelActualizarPorCampo({modelo: Usuario})
-
-
-
-export const herramientaNuevo = async ( usuario: string,  )
-
-export const usuarioIDRol = async (usuario: string, ) => {
+// Esto se puede hacer generico 
+export const herramientaIDMovimiento = async ( id: number ) => {
     try {
-        const respuesta = await Usuario.findOne({
-            where: {usuario},
+        const respuesta = await Herramienta.findOne({
+            where: { id },
             include: [{
-                model: Rol,
-                include: [{
-                    model: Permiso,
-                    through: {
-                        attributes: ['nivelAcceso']
-                    }
-                }]
+                model: MovimientoHerramienta
             }]
         })
         return respuesta
-    } catch (error) {
+    }
+    catch (error) {
+        throw error
+    }
+}
+
+// Se pasa el elemento que se va a insertar y el elemento de la tabla intermedia
+// El id a la tabla original se añade automaticamente.
+export const herramientaNuevo = async ( elemento: object, elementoMovimiento: object ) => {
+    try {
+        const respuesta = await Herramienta.build({ ...elemento })
+        await MovimientoHerramienta.create({ herramientaId: respuesta.id ,...elementoMovimiento })
+        respuesta.save();
+        return respuesta;
+
+    }
+    catch (error) {
+        console.log(error)
         throw error
     }
 }
