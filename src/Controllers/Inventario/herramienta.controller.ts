51c4ee3e051831893f55f92b9ec08a247d4d464b
Carlos Dinarte <cdinarte8@gmail.com>
2019-11-10 21:34:10 -0600

    HerramientaNuevo funcionando

--- src/Controllers/Inventario/herramienta.controller.ts
+++ src/Controllers/Inventario/herramienta.controller.ts
@@ -1,14 +1,16 @@
-import {factoryModelNuevo, factoryModelActualizarId, factoryModelEliminarCondicionAnd, factoryModelTodos, factoryModelID, factoryModelActualizarPorCampoRestriccion} from '../genericos.controller'
+import { factoryModelEliminarCondicionAnd, factoryModelTodos, factoryModelID, factoryModelActualizarPorCampoRestriccion} from '../genericos.controller'
 import { Herramienta, MovimientoHerramienta } from '../../Models/Inventario/Herramienta'
-import {Rol, Permiso, RolPermiso} from '../../Models/Usuario/RolPermiso'
+import { Insumo } from '../../Models/Inventario/Insumo'
+
 
 // Terminados
 export const herramientaTodos = factoryModelTodos({modelo: Herramienta})
-export const herramientoID = factoryModelID({modelo: Herramienta})
+export const herramientaID = factoryModelID({modelo: Herramienta})
 export const herramientaActualizar = factoryModelActualizarPorCampoRestriccion({modelo: Herramienta}) // esto no permite actualizar cantidad
+export const herramientaEliminar = factoryModelEliminarCondicionAnd({modelo: Herramienta})
 
 // Esto se puede hacer generico 
-export const herramientaIDMovimiento = async ( id: number ) => {
+export const herramientaIDMovimiento = async ( id ) => {
     try {
         const respuesta = await Herramienta.findOne({
             where: { id },
@@ -27,14 +29,30 @@ export const herramientaIDMovimiento = async ( id: number ) => {
 // El id a la tabla original se añade automaticamente.
 export const herramientaNuevo = async ( elemento: object, elementoMovimiento: object ) => {
     try {
-        const respuesta = await Herramienta.build({ ...elemento })
-        await MovimientoHerramienta.create({ herramientaId: respuesta.id ,...elementoMovimiento })
-        respuesta.save();
+        const respuesta = await Herramienta.create({ movimiento: [elementoMovimiento], ...elemento }, {
+            include: [{
+                model: MovimientoHerramienta,
+                as: 'movimiento'
+            }]
+        })
         return respuesta;
-
     }
     catch (error) {
         console.log(error)
+        throw error
+    }
+}
+
+export const herramientaIDInsumos = async (id: number) => {
+    try {
+        const respuesta = await Herramienta.findOne({
+            where: {id},
+            include: [{
+                model: Insumo   
+            }]
+        })
+        return respuesta
+    } catch (error) {
         throw error
     }
 }
